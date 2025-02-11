0d54068decddd4e3412453979750f6186db82018
AhegaoChan177013 <2436162@cegepba.qc.ca>
2025-02-11 11:40:35 -0500

    Creation classe personnage et get/set

---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+
+        int pointDeVie;
+        int puissanceAttaque;
+
+        public int PointDeVie 
+        {
+
+            get
+            {
+                return pointDeVie;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    pointDeVie = 0;
+                }
+                else
+                {
+                    pointDeVie = value;
+                }
+            }
+
+        }
+        public int PuissanceAttaque { get => puissanceAttaque; set => puissanceAttaque = value; }
+
+    }
+}
